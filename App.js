0bd8eafaccca225b70203c2b435e990c15b57dc4
CameronG <94017817+EdgarAllenBro@users.noreply.github.com>
2022-03-01 14:37:27 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react'
+import './App.css';
+// import SpellDetails from './spelldetails';
+
+function App() {
+  const [filteredSpells,setFilter] = useState([])
+  const [spells, setSpells] = useState([])
+  const [details,setDetails] = useState([])
+  const [spellName,setSpellName] = useState('')
+const filter = (event)=>{
+  let name = event.target.value
+setFilter(spells.filter(spell => spell.includes(name.charAt(0).toUpperCase()+name.slice(1))))
+}
+
+fetch('https://www.dnd5eapi.co/api/spells')
+      .then(res=>res.json())
+      .then((data)=> {
+        let spellNames = []
+        data.results.forEach((e)=>{
+          spellNames.push(e.name)
+        })
+        setSpells(spellNames)
+      }).catch(err=>console.log(err))
+
+const searchFor = (event)=>{
+  const search = event.target.innerHTML.toLowerCase().replace(/\s/g,'-').replace(/\//g,'-')
+  fetch(`https://www.dnd5eapi.co/api/spells/${search}`)
+  .then(res => res.json())
+  .then(results => { 
+    setDetails(results.desc)
+    setSpellName(results.name)
+  })
+}
+return(
+  <div >
+  <h1>spell list</h1>
+  <input onChange={filter} placeholder='Search' type='text'/>
+  <div id='book'>
+    <div className='spellsBox'>
+  {filteredSpells.map((e,i)=>{
+    return <p className='spell' onClick={searchFor} key={i}>{e}</p>
+  })}
+    </div>
+    <div className='detailsBox'>
+  <h2>{spellName}</h2>
+  <p>{details}</p>
+    </div>
+  </div>
+  </div>
+)
+}
+
+
+export default App;
